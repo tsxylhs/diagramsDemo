373be32b5abf066855617393d703812350f8fb02
tsxylhs <tsxylh@outlook.com>
2022-11-09 10:34:47 +0800

    first commit

---
+++ diagramsDemo/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace diagramsDemo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
