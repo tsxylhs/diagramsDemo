c3ca81dc4042158b5e3f464e0c75ac6b6009829b
tsxylhs <tsxylh@outlook.com>
2022-11-09 10:51:19 +0800

    add ddragAndDrop

---
+++ diagramsDemo/Pages/BotAnswerNode.cs
@@ -0,0 +1,12 @@
+﻿using Blazor.Diagrams.Core.Geometry;
+using Blazor.Diagrams.Core.Models;
+
+namespace diagramsDemo.Pages
+{
+    public class BotAnswerNode : NodeModel
+    {
+        public BotAnswerNode(Blazor.Diagrams.Core.Geometry.Point position = null) : base(position) { }
+
+        public string Answer { get; set; }
+    }
+}
---
+++ diagramsDemo/Pages/DragAndDrop.razor.cs
@@ -0,0 +1,41 @@
+﻿using Blazor.Diagrams;
+using Blazor.Diagrams.Core.Models;
+
+namespace diagramsDemo.Pages
+{
+    public partial class DragAndDrop
+    {
+        private readonly BlazorDiagram _blazorDiagram = new BlazorDiagram();
+        private int? _draggedType;
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            // LayoutData.Title = "Drag & Drop";
+            // LayoutData.Info = "A very simple drag & drop implementation using the HTML5 events.";
+            // LayoutData.DataChanged();
+
+            _blazorDiagram.RegisterComponent<BotAnswerNode, BotAnswerWidget>();
+        }
+
+        private void OnDragStart(int key)
+        {
+            // Can also use transferData, but this is probably "faster"
+            _draggedType = key;
+        }
+
+        private void OnDrop(Microsoft.AspNetCore.Components.Web.DragEventArgs e)
+        {
+            if (_draggedType == null) // Unkown item
+                return;
+
+            var position = _blazorDiagram.GetRelativeMousePoint(e.ClientX, e.ClientY);
+            var node = _draggedType == 0 ? new NodeModel(position) : new BotAnswerNode(position);
+            node.AddPort(PortAlignment.Top);
+            node.AddPort(PortAlignment.Bottom);
+            _blazorDiagram.Nodes.Add(node);
+            _draggedType = null;
+        }
+    }
+}
